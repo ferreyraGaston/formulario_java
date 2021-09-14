499ce0ce021c7f67858be38005dab7fbdbbd7c4c
Gastonloco <ferreyragaston351@gmail.com>
2021-09-13 22:13:34 -0300

    subir archivos_ primeros pasos

---
+++ src/App.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+       
+        for (double y=0.1;y<1.0;y+=0.1)
+        System.out.println(y);
+
+
+
+
+
+
+
+        
+}
+}
